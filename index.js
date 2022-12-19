7f6c23c65fb2e54145a8a33d208301f6103b3bf5
higorrabelo <higor.rabelo@gmail.com>
2022-12-19 19:03:48 -0300

    mudanças de layout e nas rotas

--- index.js
+++ index.js
@@ -5,17 +5,20 @@ const initTemplate = require('./helpers');
 const mongoose = require("mongoose");
 const usuariosModel = require('./models/usuarios');
 const tarefasModel = require('./models/tarefas');
-const crypto = require('crypto-js');
+const crypto = require('crypto-js');//chamando a biblioteca crypto-js para harsh de senhas
 
-mongoose.connect("mongodb://localhost:27017/agenda")
+mongoose.connect("mongodb://localhost:27017/agenda")//conexão com o banco mongodb agenda
 
-app.use(express.static("public"))
+app.use(express.static("public"))//Atrelando a pasta public com os arquivos css e js para o frontend
 
-initTemplate(app)
+initTemplate(app)//Iniciando Handlebars
 
+//chamando a biblioteca body-parser para captura de dados dos formulários
 app.use(bodyParser.urlencoded({extended:false}))
 app.use(bodyParser.json());
-//findOneAndRmove
+
+//findOneAndRemove função para remoção de valores do banco
+
 app.get("/",function(req,resp){
     resp.render("index",{layouts:"Home"});
 })
@@ -24,9 +27,6 @@ app.get("/agenda",function(req,resp){
     Tarefas.find({},function(err,tarefa){
         resp.render("agenda",{title:"Agenda",tarefas:tarefa});
     })
-})
-app.get("/cadastro_usuario",function(req,resp){
-    resp.render("cadastro_usuario");
 })
 app.get("/cadastro_usuario",function(req,resp){
     resp.render("cadastro_usuario");
@@ -44,33 +44,29 @@ app.post("/cadastro_user",function(req,resp){
     });
     usuario.save()
     resp.redirect("/agenda");  
-})
-app.post("/cad_tarefa",function(req,resp){
-    var titulo = req.body.titulo;
-    var descricao = req.body.descricao;
-    var usuario = req.body.usuario;
-    const Tarefas = mongoose.model("Tarefas",tarefasModel);
-
-    const tarefa = new Tarefas({
-            titulo:titulo,
-            descricao:descricao,
-            usuario:usuario
-    });
-
-    tarefa.save()
-    resp.redirect("/agenda");  ;
-})
-
+});
 app.get("/cadastro_tarefa",function(req,resp){
     var Usuarios = mongoose.model("Usuarios",usuariosModel);
     Usuarios.find({},function(err,usuario){
         resp.render("cadastro_tarefa",{title:"Tarefas",usuarios:usuario});
     })
-})
+});
+app.post("/cad_tarefa",function(req,resp){
+    var titulo = req.body.titulo;
+    var descricao = req.body.descricao;
+    var usuario = req.body.usuario;
+    const Tarefas = mongoose.model("Tarefas",tarefasModel);
+    const tarefa = new Tarefas({
+            titulo:titulo,
+            descricao:descricao,
+            usuario:usuario
+    });
+    tarefa.save()
+    resp.redirect("/agenda");  ;
+});
 app.get("/contato",function(req,resp){
     resp.render("contato");
-})
-
+});
 app.listen(8080,function(){
     console.log("Servidor Ativo")
-})
+});
